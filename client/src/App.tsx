fb829ef33897263a7ff8e257d81c974cb9f23136
Pavel C <pavelcapet@gmail.com>
2024-07-25 14:16:59 +0300

    change api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// class App {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.tsx</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 function App() {
   return (
@@ -19,6 +42,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Some text</p>
     </div>
   );
 }
